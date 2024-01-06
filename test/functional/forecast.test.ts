75b382eb2cf55a04bef2676eb6a91a1184f06e39
Vinícius Inácio <vinicius.silva@grupobrisanet.com.br>
2024-01-06 16:26:33 -0300

    did the setup for our app in test and changed the package.json to be equal to the course so we resolve some outdated packages and the first test passed

--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -1,10 +1,8 @@
-import supertest from 'supertest';
-
 describe("Beach forecast functional test", () => {
     it("Should return a forecast with just a few times", async () => {
-        const { body, status } = await supertest(app).get("/forecast"); 
+        const { body, status } = await global.testRequest.get("/forecast/"); 
         expect(status).toBe(200);
         const expected = {};
-        expect(body).toBe(expected);
+        expect(body).toEqual(expected);
     });
 });
